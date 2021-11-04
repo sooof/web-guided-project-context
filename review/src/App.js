083ea013ff5e2fb9e887eb71fcdfb8f2c8660f8f
Warren C. Longmire <warren.longmire@lambdaschool.com>
2021-11-04 12:32:19 -0400

    lecture work-in-progress

--- review/src/App.js
+++ review/src/App.js
@@ -4,6 +4,13 @@ import React from 'react';
 const SubComp1 = () => {
     return (<div className="component">
         <h1>SubComp1</h1>
+        <SubComp2/>
+    </div>)
+}
+
+const SubComp2 = () => {
+    return (<div className="component">
+        <h1>SubComp2</h1>
     </div>)
 }
 
@@ -11,6 +18,7 @@ const SubComp1 = () => {
 const App = ()=> {
     return(<div className="App component">
         <h1>Main App</h1>
+        <SubComp1/>
     </div>);
 };
 
