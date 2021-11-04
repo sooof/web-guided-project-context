16f786d7bbc93d6e650a33002d1f92d82946460f
Warren C. Longmire <warren.longmire@lambdaschool.com>
2021-11-04 13:23:51 -0400

    lecture work-in-progress

--- followAlong/src/components/FamilyTree.js
+++ followAlong/src/components/FamilyTree.js
@@ -3,13 +3,13 @@ import React, { useContext } from "react";
 import Parents from "./Parents";
 import Siblings from "./Siblings";
 
-import FamilyContext from './../App';
+import { FamilyContext } from './../App';
 
 export default function FamilyTree(props) {
   const family = useContext(FamilyContext);
 
   console.log(family);
-  
+
   return (
     <section className="FamilyTree">
       <h1>{props.family.familyName}</h1>
