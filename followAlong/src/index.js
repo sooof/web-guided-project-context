e3ba1e08197e0d7d30685ff45579b77a0eb73fda
Warren C. Longmire <warren.longmire@lambdaschool.com>
2021-11-04 13:35:28 -0400

    lecture work-in-progress

--- followAlong/src/index.js
+++ followAlong/src/index.js
@@ -11,4 +11,5 @@ const [families] = useState(data);
 const [activeFamily, setActiveFamily] = useState(families[0]);
 
 const rootElement = document.getElementById("root");
+
 ReactDOM.render(<FamilyContext.Provider value={{families, activeFamily}}><App /></FamilyContext.Provider>, rootElement);
