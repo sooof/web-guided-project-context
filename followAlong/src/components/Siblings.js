4356731a6da952bae03b68d6b433fd46e95b8ae5
Warren C. Longmire <warren.longmire@lambdaschool.com>
2021-11-04 13:32:24 -0400

    lecture work-in-progress

--- followAlong/src/components/Siblings.js
+++ followAlong/src/components/Siblings.js
@@ -2,9 +2,11 @@ import React, { useContext } from "react";
 import FamilyContext from './../contexts/FamilyContext';
 
 const Siblings = (props) => {
+  const family = useContext(FamilyContext);
+
   return (
     <section className="parents">
-      {props.family.siblings.map((p) => (
+      {family.siblings.map((p) => (
         <div className="person" key={p.name}>
           <img width="150" src={p.img} alt={p.name} />
           <strong>{p.name}</strong>
