40ff4e1d624eb7606b6b1c66b8c61dbc584101f0
Warren C. Longmire <warren.longmire@lambdaschool.com>
2021-11-04 13:24:09 -0400

    lecture work-in-progress

--- followAlong/src/components/FamilyTree.js
+++ followAlong/src/components/FamilyTree.js
@@ -5,24 +5,24 @@ import Siblings from "./Siblings";
 
 import { FamilyContext } from './../App';
 
-export default function FamilyTree(props) {
+export default function FamilyTree() {
   const family = useContext(FamilyContext);
 
   console.log(family);
 
   return (
     <section className="FamilyTree">
-      <h1>{props.family.familyName}</h1>
+      <h1>{family.familyName}</h1>
       <h2>Parents</h2>
 
       {/* Pass data down as a prop */}
-      <Parents family={props.family} />
+      <Parents family={family} />
 
       <div className="spacer" />
       <h2>Siblings</h2>
 
       {/* Siblings will consume data from Context using render props */}
-      <Siblings family={props.family} />
+      <Siblings family={family} />
     </section>
   );
 }
