7d5b3d1ae4792b98c04f608868a896058c4f20b2
sooof <sooof@me.com>
2021-11-04 23:15:03 -0700

    Review: use createContext send to SubComp3

--- review/src/App.js
+++ review/src/App.js
@@ -1,27 +1,35 @@
-import React, { useState } from 'react';
+import React, { useState, createContext, useContext } from 'react';
 import data from  './data'
 // console.log(data)
+
+const PersonContext = createContext();
+console.log("PersonContext = ", PersonContext)
 
 const App = ()=> {
     const [person, setPerson] = useState(data)
     console.log("APP ",person)
-    return(<div className="App component">
+    return(
+    <div className="App component">
         <h1>Main App</h1>
-        <SubComp1 person={person} setPerson={setPerson} />
-    </div>);
+        <PersonContext.Provider  value= {{person, setPerson}}>
+            <SubComp1  />
+        </PersonContext.Provider>
+    </div>
+  
+    );
 };
 
 const SubComp1 = (props) => {
-    const {person, setPerson } = props
-    console.log("SubComp1 ", person)
+    const {person} = useContext(PersonContext)
+    console.log("context = ", person)
     return (<div className="component">
         <h1>SubComp1</h1>
         <p>{`${person.name.title} ${person.name.first}`}</p>
-        <SubComp2 person={person} setPerson={setPerson} />
+        <SubComp2  />
     </div>)
 }
 const SubComp2 = (props) => {
-    const {person, setPerson} = props
+    const {person} = useContext(PersonContext)
     console.log("SubComp2 ", person)
 
     const handleClick = () => {
@@ -39,11 +47,11 @@ const SubComp2 = (props) => {
     return (<div className="component">
         <h1>SubComp2</h1>
         <button onClick={handleClick}>Change Location</button>
-        <SubComp3 person={person} setPerson={setPerson} />
+        <SubComp3  />
     </div>)
 }
 const SubComp3 = (props) => {
-    const {person, setPerson} = props 
+    const {person} = useContext(PersonContext)
 
     const handleClick = () => {
         setPerson({
