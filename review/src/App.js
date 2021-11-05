0af65b163d3e6989662b5e6c8943c82c3d1015c9
sooof <sooof@me.com>
2021-11-04 23:10:38 -0700

    Review: use createContext #1

--- review/src/App.js
+++ review/src/App.js
@@ -1,9 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
+import data from  './data'
+console.log(data)
 
 const App = ()=> {
+    const [person, setPerson] = useState(data)
+    console.log(person)
     return(<div className="App component">
         <h1>Main App</h1>
+        <SubComp1/>
     </div>);
 };
+
+const SubComp1 = () => {
+    return (<div className="component">
+        <h1>SubComp1</h1>
+        <SubComp2/>
+    </div>)
+}
+const SubComp2 = () => {
+    return (<div className="component">
+        <h1>SubComp2</h1>
+        <SubComp3/>
+    </div>)
+}
+const SubComp3 = () => {
+    return (<div className="component">
+        <h1>SubComp3</h1>
+    </div>)
+}
+
+
+
 
 export default App;
