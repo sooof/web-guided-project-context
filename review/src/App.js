24fe4beed7a5d1db5f74647612ae34aa5ef0f969
sooof <sooof@me.com>
2021-11-04 23:18:03 -0700

    Review:  SubComp1  use Tow context

--- review/src/App.js
+++ review/src/App.js
@@ -4,6 +4,7 @@ import data from  './data'
 
 const PersonContext = createContext();
 console.log("PersonContext = ", PersonContext)
+const DogContext = createContext();
 
 const App = ()=> {
     const [person, setPerson] = useState(data)
@@ -11,8 +12,10 @@ const App = ()=> {
     return(
     <div className="App component">
         <h1>Main App</h1>
-        <PersonContext.Provider  value= {{person, setPerson}}>
-            <SubComp1  />
+        <PersonContext.Provider  value= {{person, setPerson, name: "warren", arr:[1,2,3,4,5]}}>
+            <DogContext.Provider>
+                <SubComp1  />
+            </DogContext.Provider>
         </PersonContext.Provider>
     </div>
   
@@ -20,16 +23,20 @@ const App = ()=> {
 };
 
 const SubComp1 = (props) => {
-    const {person} = useContext(PersonContext)
-    console.log("context = ", person)
+    const {person, name} = useContext(PersonContext)
+    // const context = useContext(PersonContext)
+    // console.log("context = ", context)
+    console.log("context = ", name)
     return (<div className="component">
         <h1>SubComp1</h1>
-        <p>{`${person.name.title} ${person.name.first}`}</p>
+        <p>{`${person.name.title} ${person.name.first}   ${name} ` }</p>
+        {/* <p>{`${context.person.name.title} ${context.person.name.first}   ${context.name} ` }</p> */}
+        {/* <p>{`${useContext(PersonContext).person.name.title} ${useContext(PersonContext).person.name.first}`}</p> */}
         <SubComp2  />
     </div>)
 }
 const SubComp2 = (props) => {
-    const {person} = useContext(PersonContext)
+    const {person, setPerson} = useContext(PersonContext)
     console.log("SubComp2 ", person)
 
     const handleClick = () => {
@@ -51,7 +58,7 @@ const SubComp2 = (props) => {
     </div>)
 }
 const SubComp3 = (props) => {
-    const {person} = useContext(PersonContext)
+    const {person, setPerson, arr} = useContext(PersonContext)
 
     const handleClick = () => {
         setPerson({
@@ -64,6 +71,8 @@ const SubComp3 = (props) => {
         });
     }
     console.log("SubComp3 ", person)
+
+    console.log("arr = ", arr)
     return (<div className="component">
         <h1>SubComp3</h1>
         <p>{`${person.location.street} ${person.location.scity} ${person.location.postcode}`}</p>
