7d54ce423c9fa48fc5bc13d1531067acd0f5f011
sooof <sooof@me.com>
2021-12-09 10:31:18 -0800

    Review: use props pass data  && chenge data

--- review/src/App.js
+++ review/src/App.js
@@ -1,31 +1,65 @@
 import React, { useState } from 'react';
 import data from  './data'
-console.log(data)
+// console.log(data)
 
 const App = ()=> {
     const [person, setPerson] = useState(data)
-    console.log(person)
+    console.log("Func---App ", person)
     return(<div className="App component">
         <h1>Main App</h1>
-        <SubComp1/>
+        <SubComp1 person={person} setPerson={setPerson} />
     </div>);
 };
 
-const SubComp1 = () => {
+const SubComp1 = (props) => {
+    const {person, setPerson } = props
+    console.log("Fubc---SubComp1 ", person)
     return (<div className="component">
         <h1>SubComp1</h1>
-        <SubComp2/>
+        <p>{`${person.name.title} ${person.name.first}`}</p>
+        <SubComp2 person={person} setPerson={setPerson} />
     </div>)
 }
-const SubComp2 = () => {
+const SubComp2 = (props) => {
+    const {person, setPerson } = props
+    console.log("Fubc---SubComp2 ", person)
+    const handleClick = () => {
+        setPerson({
+            ...person,
+            location: {
+                street: "222",
+                city: "San ",
+                state: "CA",
+                postcode: "94706"
+
+            }
+        });
+    }
     return (<div className="component">
         <h1>SubComp2</h1>
-        <SubComp3/>
+        <button onClick={handleClick}>Change Location</button>
+        <SubComp3 person={person} setPerson={setPerson}/>
     </div>)
 }
-const SubComp3 = () => {
+const SubComp3 = (props) => {
+    const {person, setPerson} = props 
+
+    const handleClick = () => {
+        setPerson({
+            ...person,
+            name: {
+                title: "222",
+                first: "San ",
+                last: "CA",
+            }
+        });
+    }
+    console.log("Fubc---SubComp3 ", person)
     return (<div className="component">
         <h1>SubComp3</h1>
+        <p>{`${person.location.street} ${person.location.scity} ${person.location.postcode}`}</p>
+        <button onClick={handleClick}>Change Name</button>
     </div>)
 }
+
 export default App;
